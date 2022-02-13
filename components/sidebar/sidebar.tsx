0b3035f9be89c7a0769954c51d86d21c8c54244f
Tyler Nevell <tylernevell@gmail.com>
2022-02-13 09:37:22 -0500

    successfully pull playlists

--- components/sidebar/sidebar.tsx
+++ components/sidebar/sidebar.tsx
@@ -1,16 +1,27 @@
-import {
-  HomeIcon,
-  SearchIcon,
-  LibraryIcon,
-  PlusCircleIcon,
-  HeartIcon,
-  RssIcon,
-} from '@heroicons/react/outline';
-import { SidebarButton } from './sidebar-button';
+import { useSession } from 'next-auth/react';
+import { useEffect, useState } from 'react';
+import { useSpotify } from '../../hooks/useSpotify';
 import { SidebarNavigation } from './sidebar-navigation';
 import { SidebarPlaylists } from './sidebar-playlists';
 
 const Sidebar = () => {
+  const [playlists, setPlayLists] = useState<
+    SpotifyApi.PlaylistObjectSimplified[]
+  >([]);
+
+  const spotifyApi = useSpotify();
+  const { data: session, status } = useSession();
+
+  useEffect(() => {
+    if (spotifyApi.getAccessToken()) {
+      spotifyApi.getUserPlaylists().then((data) => {
+        setPlayLists(data.body.items);
+      });
+    }
+  }, [session, spotifyApi]);
+
+  console.log(playlists);
+
   return (
     <aside className="text-gray-500 p-5 text-sm border-r border-gray-900 overflow-y-scroll scrollbar-hide h-screen ">
       <SidebarNavigation />
