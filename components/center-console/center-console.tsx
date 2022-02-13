de3587d1783f3f3e4563f1e6a0993fe1dc901ccf
Tyler Nevell <tylernevell@gmail.com>
2022-02-12 19:46:35 -0500

    add hook to call api on page rerender.

--- components/center-console/center-console.tsx
+++ components/center-console/center-console.tsx
@@ -1,8 +1,25 @@
 import { useSession } from 'next-auth/react';
+import { useEffect, useState } from 'react';
 import { UserBadge } from './user-badge';
+import { shuffle } from 'lodash';
+
+const COLORS = [
+  'from-emerald-500',
+  'from-fuchsia-500',
+  'from-pink-500',
+  'from-orange-500',
+  'from-sky-500',
+  'from-rose-500',
+];
 
 const CenterConsole = () => {
   const { data: session } = useSession();
+
+  const [color, setColor] = useState('');
+
+  useEffect(() => {
+    setColor(shuffle(COLORS).pop()!);
+  }, []);
 
   return (
     <section className="flex-grow text-white">
@@ -11,7 +28,7 @@ const CenterConsole = () => {
         username={session?.user?.username}
       />
       <section
-        className={`flex items-end space-x-7 bg-gradient-to-b to-black from-red-500 h-80 text-white p-8`}
+        className={`flex items-end space-x-7 bg-gradient-to-b to-black ${color} h-80 text-white p-8`}
       >
         <h1>hello</h1>
       </section>
